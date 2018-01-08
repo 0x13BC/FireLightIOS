3d8b6fcb10b36137bcdd384b03ce0daa185ed36d
Developer <developer@Developers-Mac.local>
2018-01-08 16:41:10 +0100

    starting Xcode file

---
+++ FireLightIOS/FireLightIOS/DeviceList.swift
@@ -0,0 +1,21 @@
+
+struct Device : Equatable
+{
+	var name : String
+	var adress : String
+}
+
+class DeviceList
+{
+	var Devices :[Device]
+
+	func pairedDeviceList()
+	{
+
+	}
+
+	func ListListener()
+	{
+
+	}
+}
---
+++ FireLightIOS/FireLightIOS/FireLight.swift
@@ -0,0 +1,14 @@
+import LedControl
+import DeviceList
+
+
+class Firelight
+{
+	var mode :bool = true
+	var LedManager :LedControl
+	var DeviceManager :DeviceList
+	
+ 
+
+
+}
