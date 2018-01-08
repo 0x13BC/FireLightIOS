8e6c00666ad4e70c4c9cc3528f1a9001d43f5bff
Paul <cyberprime99@gmail.com>
2018-01-08 05:11:02 +0100

    Creating Basic Files
    blup!

---
+++ ConnectionBI.swift
@@ -0,0 +1,38 @@
+import CoreBluetooth
+
+class ConnectionBl
+{
+	var name : String
+	var adress : String
+
+	
+	func Connect()
+	{
+
+	}
+
+	func Disconnect()
+	{
+
+	}
+
+	func Message()
+	{
+
+	}
+
+	func send()
+	{
+
+	}
+
+	func receive()
+	{
+		
+	}
+	
+	func TestConnect()
+	{
+
+	}
+}
---
+++ LedControl.swift
@@ -0,0 +1,39 @@
+import MacTypes
+import ConnectionBl
+
+class LedControl 
+{ 
+	var RGB :[UInt8] = [255,255,255]
+	var HSL :[double] =[350,100,100]
+	var modeColors : String = "HSL"   
+	var id
+
+	var BluetoothManager :ConnectionBl
+
+	func rgbToHsl()
+	{
+
+	}
+	func HslToRgb()
+	{
+
+	}
+
+	func TurnOffLed()
+	{
+
+	}
+	func TurnOnLed()
+	{
+
+	}
+	func Wave()
+	{
+
+	}
+
+
+
+	
+}
+
